576c8c1a6811f8ef7eb5066ff35b02540cf57921
sharif <sharifpial225@gmail.com>
2020-10-28 23:56:10 -0600

    initial commit

---
+++ assignment_0/README.md
@@ -0,0 +1,3 @@
+### assignment_0
+
+This is an express app that uses jade as its templete.This app fetches data from a remote JSON file (https://www.themealdb.com/api/json/v1/1/search.php?s= "**_The name of the food porduct_**") and uses the retrived data to show the users the name, link to the video recepie and picture of the final product. 
---
+++ assignment_0/routes/omelette.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var fetch = require('node-fetch');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  var myobject = {};
+  
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Omelette')
+    .then(request => request.json())
+    .then(response => {
+      myobject = response.meals;
+      res.render('index', { "obj": myobject});
+    });
+  
+});
+
+module.exports = router;
